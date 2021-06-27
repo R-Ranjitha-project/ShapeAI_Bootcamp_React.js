bed5413a8fe04a5aa0f298f7a3a63c4aa9e69fb9
R-Ranjitha-project <67631087+R-Ranjitha-project@users.noreply.github.com>
2021-06-27 14:32:47 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Note() {
+	return (
+   <div className="note">
+       <h1>Javascript and React.js</h1>
+       <p>This Bootcamp was conducted by Shaurya Sinha</p>
+   </div>
+  );
+}
+export default Note;
